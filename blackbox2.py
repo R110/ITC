5fe922ca94ecef85cad3930e4fdbee0294cdac12
R110 <jacobs9590@gmail.com>
2017-08-29 23:07:13 -0500

    need img parameter name

--- blackbox2.py
+++ blackbox2.py
@@ -2,6 +2,7 @@ import itertools
 import requests
 import json
 import base64
+import inspect
 
 def send_request(method, params):
     url = "http://hack.israeltechallenge.com:8000/"
@@ -17,19 +18,24 @@ def send_request(method, params):
 
 response = send_request('help', [])
 print(response)
+print('-----------------------')
+# response = send_request('verify_access_token', [])
+# print(response) -> Does not exist
+# response = send_request('doc', [])
+# print(response) -> Does not exist
 
-response = send_request('login', ['ITC', 'l33#'])
-token = response['result']['token']
-
-#response = send_request('get_temporary_access_token', ['ITC', 'l33#'])
-#token = response['result']
-#print(token)
+response = send_request('get_temporary_access_token', ['ITC', 'l33#'])
+token = response['result']
+print(token)
 
 with open("input.bmp", "rb") as image_file:
     encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
 
-response = send_request('hide_secret_in_image', [token, encoded_image])
+
+response = send_request('hide_secret_in_image', {'access_token':token, 'image': encoded_image })
 print(response)
+# -->Error: rpc_function(*positional_params, **named_params)\nTypeError: hide_secret_in_image() got an unexpected keyword argument \'image\'\n'}}
+
 
 #image = base64.b64decode(response['result'].replace('\n', ''))
 #with open("output.bmp", "wb") as image_file:
